1cab2040564fad743da31db4649bf1ea90f05fc8
Gaurav Thakare <gaurav06thakare@gmail.com>
2026-07-22 20:24:42 +0530

    temporarily run database migrations on startup

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,17 @@
 from app import create_app
+from flask_migrate import upgrade
+
 
 app = create_app()
 
+
+# Temporary: run Alembic migrations on Render startup
+with app.app_context():
+    upgrade()
+
+
 if __name__ == "__main__":
     app.run(
-    debug=True,
-    use_reloader=False
-)
+        debug=False,
+        use_reloader=False
+    )
