ccae113895e4267bce45741fafd9ff3386f8a0df
Gaurav Thakare <gaurav06thakare@gmail.com>
2026-07-22 20:28:08 +0530

    fix render startup migration

--- backend/run.py
+++ backend/run.py
@@ -1,17 +1,9 @@
 from app import create_app
-from flask_migrate import upgrade
-
 
 app = create_app()
 
-
-# Temporary: run Alembic migrations on Render startup
-with app.app_context():
-    upgrade()
-
-
 if __name__ == "__main__":
     app.run(
-        debug=False,
+        debug=True,
         use_reloader=False
     )
